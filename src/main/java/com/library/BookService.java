90d50567b8ccdcb69bc8a26e4c53006bc2470f46
Simruti <gidwanisimruti@gmail.com>
2024-08-31 21:43:12 +0530

    Add validation to check for duplicate records.

--- src/main/java/com/library/BookService.java
+++ src/main/java/com/library/BookService.java
@@ -1,9 +1,8 @@
 package com.library;
 
 import java.sql.Connection;
-import java.sql.Date;
-import java.sql.DriverManager;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
@@ -14,10 +13,25 @@ public class BookService {
     {
         try
         {
-            Class.forName("com.mysql.cj.jdbc.Driver");
-            Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/library","root","");
-            String query = "INSERT INTO book(isbn,title,author,publication_year) VALUES (?,?,?,?)";
-            PreparedStatement statement = con.prepareStatement(query);
+            Connection con = DbConnectivity.getConnection();
+            String checkQry = "SELECT * FROM book WHERE isbn = ?";
+            PreparedStatement checkStatement = con.prepareStatement(checkQry);
+            ResultSet resultSet = null;
+            if(book == null)
+            {
+                return;
+            }
+            else{
+                checkStatement.setInt(1,book.getIsbn());
+                resultSet = checkStatement.executeQuery();
+            }
+            if(resultSet.next() && resultSet.getInt(1)>0)
+            {
+                System.out.println("Book already exists in the database");
+                return;
+            }
+            String add_book = "INSERT INTO book(isbn,title,author,publication_year) VALUES (?,?,?,?)";
+            PreparedStatement statement = con.prepareStatement(add_book);
             statement.setInt(1,book.getIsbn());
             statement.setString(2,book.getTitle());
             statement.setString(3, book.getAuthor());
@@ -30,11 +44,8 @@ public class BookService {
             con.close();
 
 
-        }catch (SQLException e)
-        {
+        }catch (SQLException e) {
             System.out.println(e.getMessage());
-        } catch (ClassNotFoundException e) {
-            System.out.println("Class Not Found Exception"+e.getMessage());
         }
     }
 
--- src/test/java/com/library/BookServiceTest.java
+++ src/test/java/com/library/BookServiceTest.java
@@ -1,15 +1,17 @@
 package com.library;
 
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
+import org.mockito.MockedStatic;
 import org.mockito.MockitoAnnotations;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
 
@@ -24,30 +26,77 @@ public class BookServiceTest {
     @Mock
     private PreparedStatement mockPreparedStatement;
 
-    @BeforeEach
-    void setUp()throws SQLException
-    {
-        MockitoAnnotations.openMocks(this);
-        bookService = new BookService();
+    @Mock
+    private PreparedStatement mockCheckPreparedStatement;
 
-        when(mockConnection.prepareStatement(any(String.class))).thenReturn(mockPreparedStatement);
+    @Mock
+    private PreparedStatement mockInsertPreparedStatement;
+
+    @Mock
+    private ResultSet mockResultSet;
+
+    private MockedStatic<DbConnectivity> mockedStatic;
+
+    @BeforeEach
+    void setUp() throws SQLException {
+        MockitoAnnotations.openMocks(this);
+        mockedStatic = mockStatic(DbConnectivity.class);
+        mockedStatic.when(DbConnectivity::getConnection).thenReturn(mockConnection);
+        when(mockConnection.prepareStatement("SELECT * FROM book WHERE isbn = ?")).thenReturn(mockCheckPreparedStatement);
+        when(mockCheckPreparedStatement.executeQuery()).thenReturn(mockResultSet);
+
+        when(mockConnection.prepareStatement("INSERT INTO book(isbn,title,author,publication_year) VALUES (?,?,?,?)"))
+                .thenReturn(mockInsertPreparedStatement);
+//        when(mockConnection.prepareStatement(any(String.class))).thenReturn(mockPreparedStatement);
+//        when(mockPreparedStatement.executeQuery()).thenReturn(mockResultSet);
+        bookService = new BookService();
+    }
+
+    @AfterEach
+    void closeMockConn() {
+        mockedStatic.close();
     }
 
     @Test
-    public void testaddBook() throws SQLException
+    public void testAddBookWithSameISBN() throws SQLException
     {
-        BookService bookService = new BookService();
         Book book = new Book(1,"Java Programming","Cay S. Horstmann",2001);
+
+        when(mockResultSet.next()).thenReturn(true);
+        //when(mockResultSet.getInt(1)).thenReturn(1);
+
         bookService.addBook(book);
 
-        verify(mockPreparedStatement,times(1)).setInt(1,book.getIsbn());
-        verify(mockPreparedStatement,times(1)).setString(2,book.getTitle());
-        verify(mockPreparedStatement,times(1)).setString(3,book.getAuthor());
-        verify(mockPreparedStatement,times(1)).setInt(4,book.getPublicationYear());
-        verify(mockPreparedStatement,times(1)).executeUpdate();
-        verify(mockPreparedStatement,times(1)).close();
-        verify(mockConnection,times(1));
+        verify(mockConnection,times(1)).prepareStatement("SELECT * FROM book WHERE isbn = ?");
+        verify(mockCheckPreparedStatement,times(1)).setInt(1,book.getIsbn());
+        verify(mockCheckPreparedStatement,times(1)).executeQuery();
+        //verify(mockCheckPreparedStatement,times(1)).executeUpdate();
+        //verify(mockInsertPreparedStatement, never()).executeUpdate();
 
+        verify(mockConnection, times(1)).close();
+    }
 
+    @Test
+    public void testAddBook() throws SQLException
+    {
+        Book book = new Book(1,"Java Programming","Cay S. Horstmann",2001);
+
+        when(mockResultSet.next()).thenReturn(true);
+        when(mockResultSet.getInt(1)).thenReturn(0);
+        bookService.addBook(book);
+
+        verify(mockConnection,times(1)).prepareStatement("SELECT * FROM book WHERE isbn = ?");
+        verify(mockCheckPreparedStatement,times(1)).setInt(1,book.getIsbn());
+        verify(mockCheckPreparedStatement,times(1)).executeQuery();
+
+        verify(mockConnection,times(1)).prepareStatement("INSERT INTO book(isbn,title,author,publication_year) VALUES (?,?,?,?)");
+        verify(mockInsertPreparedStatement,times(1)).setInt(1,book.getIsbn());
+        verify(mockInsertPreparedStatement,times(1)).setString(2,book.getTitle());
+        verify(mockInsertPreparedStatement,times(1)).setString(3,book.getAuthor());
+        verify(mockInsertPreparedStatement,times(1)).setInt(4,book.getPublicationYear());
+        verify(mockInsertPreparedStatement,times(1)).executeUpdate();
+        verify(mockInsertPreparedStatement,times(1)).close();
+        verify(mockConnection,times(1)).close();
     }
 }
+
